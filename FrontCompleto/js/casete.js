805edb61cd7629c6a14da800ebcba2837d019cfc
Tabu03 <tabulete03@gmail.com>
2023-03-16 19:38:14 +0100

    file prueba

--- FrontCompleto/js/casete.js
+++ FrontCompleto/js/casete.js
@@ -1,12 +1,15 @@
 const inputCassete = document.getElementById("inputCassete")
 const token = sessionStorage.getItem('token');
+
 const casettes = document.getElementById("casettes")
 const organos = document.getElementById("organos")
+
 const btnborrar = document.getElementById("btnborrar")
 const btnmodificar = document.getElementById("btnmodificar")
 const nuevoCassette = document.getElementById("nuevoCassette")
 const nuevaMuestra = document.getElementById("nuevaMuestra")
 
+const files = document.getElementById("files")
 console.log(token)
 
 
@@ -37,55 +40,85 @@ const cargarTodos = () => {
         headers: {
             'user-token': token,
         },
-    }).then(response=>response.json())
+    }).then(response => response.json())
 }
 
 const cargarPorOrgano = () => {
-    return fetch('http://localhost:3000/v1/casettes/organo/'+organos.value, {
+    return fetch('http://localhost:3000/v1/casettes/organo/' + organos.value, {
         method: 'GET',
         headers: {
             'user-token': token,
         },
-    }).then(response=>response.json())
+    }).then(response => response.json())
 }
 
-const imprimirCasettes=(respuesta)=>{
+const imprimirCasettes = (respuesta) => {
     while (casettes.lastElementChild) {
         casettes.removeChild(casettes.lastElementChild);
     }
 
-    let fragmento=document.createDocumentFragment()
+    let fragmento = document.createDocumentFragment()
     respuesta.forEach((casete) => {
         let tr = document.createElement("tr")
+        tr.classList.add("table__row")
         let fecha = document.createElement("td")
-        fecha.textContent=casete.fecha
+        fecha.textContent = casete.fecha
         let caracteristicas = document.createElement("td")
-        caracteristicas.textContent=casete.caracteristicas
+        caracteristicas.textContent = casete.caracteristicas
         let organo = document.createElement("td")
-        organo.textContent=casete.organo
+        organo.textContent = casete.organo
         let btn = document.createElement("button")
         btn.classList.add("button-details")
         btn.textContent = "Detalles"
+        btn.value = casete.id_casette
+        let btnCont = document.createElement("td")
+        btnCont.appendChild(btn)
 
         tr.appendChild(fecha)
         tr.appendChild(caracteristicas)
         tr.appendChild(organo)
-        tr.appendChild(btn)
+        tr.appendChild(btnCont)
 
         fragmento.appendChild(tr)
     });
     casettes.appendChild(fragmento)
 }
 
+const anadirCassette = (event) => {
+    event.preventDefault();
+    let newLogin = {
+        fecha: 2001 - 12 - 08,
+        caracteristicas: "sdfghjk",
+        observaciones: "dsfdgfhgjk",
+        imagen: files.files[0],
+        qr_casette: "a",
+        tecnicoIdTecnico: "1",
+        organo: "vejiga",
+    }
 
-document.addEventListener("DOMContentLoaded", async()=>{
-    const respuesta=await cargarTodos()
+    fetch("http://localhost:3000/v1/tecnicos/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newLogin)
+    }) //  Aqui acaba el fetch
+        .then(response => response.json())
+        .then(response => {
+            sessionStorage.setItem("token", response.succes);
+            location.replace('pages/casetes.html');
+        })
+        .catch(error => console.log(error));
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const respuesta = await cargarTodos()
     imprimirCasettes(respuesta)
 })
 
-organos.addEventListener("change",async()=>{
-    const respuesta=await cargarPorOrgano()
+organos.addEventListener("change", async () => {
+    const respuesta = await cargarPorOrgano()
     imprimirCasettes(respuesta)
 })
 
-
+nuevoCassette.addEventListener("click", anadirCassette)
